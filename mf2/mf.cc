080529ee117e5426e04b1bacfed32625e0a0128e
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-04-17 17:55:01 +0300

    initial file, just the copy of the current mf1/mf.cc file

--- mf2/mf.cc
+++ mf2/mf.cc
@@ -1,8 +1,46 @@
+#include <algorithm>
 #include "mf.h"
 
 void mf(int ny, int nx, int hy, int hx, const float* in, float* out) {
-    // FIXME
-    for (int i = 0; i < ny * nx; ++i) {
-        out[i] = in[i];
+  int px, py, n, i, j, wxmin, wxmax, wymin, wymax, wsize;
+  float* window;
+  float median, m1, m2;
+
+  // Loop through the pixels in the image
+  for (py=0; py<ny; py++) {
+    for (px=0; px<nx; px++) {
+	// limit the window, s.t. it's smaller near the boundaries
+      wxmin = (px - hx) > 0        ? (px - hx) : 0;
+      wxmax = (px + hx) < (nx - 1) ? (px + hx+1) : nx;
+      wymin = (py - hy) > 0        ? (py - hy) : 0;
+      wymax = (py + hy) < (ny - 1) ? (py + hy+1) : ny;
+
+      wsize = (wxmax - wxmin) * (wymax - wymin);
+      window = (float*) malloc(sizeof(float) * wsize);
+
+      // Store the values in window
+      n = 0;
+      for (i=wxmin; i<wxmax; i++) {
+	for (j=wymin; j<wymax; j++) {
+	  window[n++] = in[j*nx + i];
+	}
+      }
+
+      // Find the median, if window's length is even, median
+      // is the average of the two 'middle' values
+      if (wsize % 2 == 0) {
+	  std::nth_element(window, window + wsize/2, window + wsize);
+	  m2 = window[wsize/2];
+	  std::nth_element(window, (window + wsize/2)-1, window + wsize);
+	  m1 = window[wsize/2-1];
+	  median = (m1 + m2) / 2.0;
+      } else {
+	  std::nth_element(window, window + wsize/2, window + wsize);
+	  median = window[wsize/2];
+      }
+
+      out[py*nx + px] = median;
+      free(window);
     }
+  }
 }
