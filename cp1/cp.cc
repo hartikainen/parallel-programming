42e874e8a33998aeee5099f6399534e46c086eef
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-04-24 20:09:45 +0300

    Save some prints

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -36,33 +36,49 @@ double dot_product(double* v1, double* v2, int len) {
 void correlate(int ny, int nx, const float* data, float* result) {
   double row_mean, row_rss, sum, square_sum, dp;
   double X[nx*ny], v1[nx], v2[ny];
-
+  std::cout << "data: " << data[0] << " " << data[1] << " " << data[2] << " " << data[3] << "\n";
+  std::cout << "nx: " << nx << " ny: " << ny << "\n";
   for (int y=0; y<ny; y++) {
     sum = 0.0;
     square_sum = 0.0;
 
     for (int x=0; x<nx; x++) {
-      double paska = data[POINT];
-      sum += paska;
+      sum += (double) data[POINT];
     }
 
+    std::cout << "\nrow sum=" << sum;
     row_mean = sum / ((double) nx);
+
+    std::cout << "\nrow_mean=" << row_mean << "\n";
 
     for (int x=0; x<nx; x++) {
       X[POINT] = ((double) data[POINT]) - row_mean;
       square_sum += X[POINT] * X[POINT];
     }
 
+    std::cout << "X after zero meaning:\n";
+    std::cout << "X: " << X[0] << " " << X[1] << " " << X[2] << " " << X[3] << "\n";
+
     row_rss = sqrt(square_sum);
+
+    std::cout << "row_rss=" << row_rss << "\n";
 
     for (int x=0; x<nx; x++) {
       X[POINT] = X[POINT] / row_rss;
     }
 
-  }
+    std::cout << "X after unit variancing:\n";
+    std::cout << "X: " << X[0] << " " << X[1] << " " << X[2] << " " << X[3] << "\n";
 
+  }
+  std::cout << "X: " << X[0] << " " << X[1] << " " << X[2] << " " << X[3] << "\n";
+
+  printf("X:\n");
   for (int y=0; y<ny; y++) {
+    //    for (int x=0; x<y; x++) printf("%.8f ", X[POINT]);
     for (int x=y; x<ny; x++) {
+      printf("%.8f ", X[POINT]);
+
       for (int i=0; i<nx; i++) {
 	v1[i] = X[y*ny + i];
       }
@@ -74,5 +90,16 @@ void correlate(int ny, int nx, const float* data, float* result) {
       dp = dot_product(v1, v2, nx);
       result[POINT] = (float) dp;
     }
+
+    printf("\n");
+  }
+
+  printf("result:\n");
+  for (int y=0; y<ny; y++) {
+    for (int x=0; x<y; x++) printf(".......... ");
+    for (int x=y; x<nx; x++) {
+      printf("%.8f ", result[POINT]);
+    }
+    printf("\n");
   }
 }
