57c180d115af343571b466b4494e50e821ba4649
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-04-25 02:36:25 +0300

    PASSED

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -1,9 +1,7 @@
 #include "cp.h"
-#include <math.h>
+#include <cmath>
 #include <stdio.h>
 #include <iostream>
-
-#define POINT y*nx + x
 
 double get_mean(const float* row, int nx) {
   double sum = 0.0, mean;
@@ -38,66 +36,38 @@ void correlate(int ny, int nx, const float* data, float* result) {
   double row_mean, row_rss, sum, square_sum, dp;
   double X[nx*ny], v1[max_dim], v2[max_dim];
 
-  std::cout << "nx: " << nx << " ny: " << ny << "\n";
-  std::cout << "data: " << data[0] << " " << data[1] << " " << data[2] << " " << data[3] << " " << data[4] << "\n";
-  std::cout << "      " << data[5] << " " << data[6] << " " << data[7] << " " << data[8] << " " << data[9] <<"\n";
-
   for (int y=0; y<ny; y++) {
     sum = 0.0;
     square_sum = 0.0;
 
     for (int x=0; x<nx; x++) {
-      sum += (double) data[POINT];
+      sum += (double) data[y*nx + x];
     }
 
-    std::cout << "\nrow sum=" << sum;
     row_mean = sum / ((double) nx);
 
-    std::cout << "\nrow_mean=" << row_mean << "\n";
-
     for (int x=0; x<nx; x++) {
-      X[POINT] = ((double) data[POINT]) - row_mean;
-      square_sum += X[POINT] * X[POINT];
+      X[y*nx + x] = ((double) data[y*nx + x]) - row_mean;
+      square_sum += X[y*nx + x] * X[y*nx + x];
     }
 
-    std::cout << "X after zero meaning:\n";
-    std::cout << "X: " << X[0] << " " << X[1] << " " << X[2] << " " << X[3] << " " << X[4] << "\n";
-    std::cout << "   " << X[5] << " " << X[6] << " " << X[7] << " " << X[8] << " " << X[9] <<"\n";
-
-    row_rss = sqrt(square_sum);
-
-    std::cout << "row_rss=" << row_rss << "\n";
+    row_rss = std::sqrt(square_sum);
 
     for (int x=0; x<nx; x++) {
-      X[POINT] = X[POINT] / row_rss;
+      X[y*nx + x] = X[y*nx + x] / row_rss;
     }
-
-    std::cout << "X: " << X[0] << " " << X[1] << " " << X[2] << " " << X[3] << " " << X[4] << "\n";
-    std::cout << "   " << X[5] << " " << X[6] << " " << X[7] << " " << X[8] << " " << X[9] <<"\n";
-
   }
 
   for (int y=0; y<ny; y++) {
     for (int x=y; x<ny; x++) {
 
-      for (int i=0; i<ny; i++) {
-	v1[i] = X[y*nx + i];
+      for (int i=0; i<nx; i++) {
+	v1[i] = X[x*nx + i];
 	v2[i] = X[y*nx + i];
       }
 
       dp = dot_product(v1, v2, max_dim);
-      result[POINT] = (float) dp;
+      result[y*ny + x] = (float) dp;
     }
-
-    printf("\n");
-  }
-
-  printf("result:\n");
-  for (int y=0; y<ny; y++) {
-    for (int x=0; x<y; x++) printf(".......... ");
-    for (int x=y; x<ny; x++) {
-      printf("%.8f ", result[POINT]);
-    }
-    printf("\n");
   }
 }
