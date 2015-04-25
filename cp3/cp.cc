4143722b0d81a3c19eafa651b7aa3ad4475de581
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-04-25 19:46:02 +0300

    cp3 PASSED

--- cp3/cp.cc
+++ cp3/cp.cc
@@ -19,9 +19,9 @@ double get_sum(double4_t* row, int n) {
   return sum;
 }
 
-double4_t get_root_square_sum(double4_t* row, int n) {
+double get_root_square_sum(double4_t* row, int n) {
   double row_ss = 0.0, row_rss;
-  double4_t tmp4, row_ss4 = {0.0};
+  double4_t tmp4, row_ss4 = {0.0, 0.0, 0.0, 0.0};
 
   for (int i=0; i<n; i++) {
     tmp4 = row[i];
@@ -34,8 +34,7 @@ double4_t get_root_square_sum(double4_t* row, int n) {
 
   row_rss = std::sqrt(row_ss);
 
-  double4_t row_rss4 = {row_rss, row_rss, row_rss, row_rss};
-  return row_rss4;
+  return row_rss;
 }
 
 void correlate(int ny, int nx, const float* data, float* result) {
@@ -44,15 +43,15 @@ void correlate(int ny, int nx, const float* data, float* result) {
   double4_t* X = double4_alloc(nnx*ny);
 
   // Copy the data into double4_t array X
+#pragma omp parallel for
   for (int y=0; y<ny; y++) {
     for (int x1=0; x1<nnx-1; x1++) {
       for (int x2=0; x2<4; x2++) {
 	X[y*nnx + x1][x2] = data[y*nx + x1*4 + x2];
-	printf("times here: %d", x2);
       }
     }
 
-    for (int x=0; x<xpad; x++) {
+    for (int x=0; x<4-xpad; x++) {
       X[y*nnx + nnx-1][x] = data[y*nx + (nnx - 1) * 4 + x];
     }
     // Initialize the remaining 'right' padding to 0.0
@@ -81,21 +80,16 @@ void correlate(int ny, int nx, const float* data, float* result) {
 
 #pragma omp parallel for
   for (int y=0; y<ny; y++) {
-    double row_sum, row_mean;
-    double4_t row_rss;
+    double row_sum, row_mean, row_rss;
 
     row_sum = get_sum(&X[y*nnx], nnx);
     row_mean = row_sum / (double)nx;
 
-    double4_t row_mean4 = {row_mean, row_mean, row_mean, row_mean};;
-
     for (int x1=0; x1<nnx-1; x1++) {
-      for (int x2=0; x2<4; x2++) {
-	X[y*nnx + x1] = X[y*nnx + x1] - row_mean4;
-      }
+      X[y*nnx + x1] = X[y*nnx + x1] - row_mean;
     }
 
-    for (int x=0; x<xpad; x++) {
+    for (int x=0; x<4-xpad; x++) {
       X[y*nnx + nnx-1][x] = data[y*nx + (nnx - 1) * 4 + x] - row_mean;
     }
 
@@ -106,24 +100,27 @@ void correlate(int ny, int nx, const float* data, float* result) {
     }
   }
 
-  // std::cout << "\nzero mean X: \n";
-  // for (int y=0; y<ny; y++) {
-  //   for (int x1=0; x1<nnx; x1++) {
-  //     for (int x2=0; x2<4; x2++) {
-  // 	printf("%f ", X[y*nnx + x1][x2]);
-  //     }
-  //   }
-  //   printf("\n");
-  // }
+#pragma omp parallel for schedule(static, 1)
+  for (int y=0; y<ny; y++) {
+    //printf("\n\nrow incoming: \n\n");
+    for (int x=y; x<ny; x++) {
+      double4_t r = {0.0, 0.0, 0.0, 0.0};
+      double4_t* row1 = &X[y*nnx];
+      double4_t* row2 = &X[x*nnx];
 
-// #pragma omp parallel for schedule(static, 1)
-//   for (int y=0; y<ny; y++) {
-//     for (int x=y; x<ny; x++) {
-//       double r = 0.0;
-//       for (int i=0; i<nx; i++) {
-// 	r += X[x*nx + i] * X[y*nx + i];
-//       }
-//       result[y*ny + x] = r;
-//     }
-//   }
+      for (int i=0; i<nnx; i++) {
+	//r += X[x*nnx + i] * X[y*nnx + i];
+	r += row1[i] * row2[i];
+
+	// for (int x2=0; x2<4; x2++) {
+	//   printf("row1: %f, row2: %f\n", row1[i][x2], row2[i][x2]);
+	// }
+      }
+      double t = 0.0;
+      for (int i=0; i<4; i++) {
+	t += r[i];
+      }
+      result[y*ny + x] = t;
+    }
+  }
 }
