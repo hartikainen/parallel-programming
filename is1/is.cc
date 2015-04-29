3c6c0835e63450814220bf4609f8eabd96370a2a
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-04-29 20:01:28 +0300

    Removed useless comments and prints

--- is1/is.cc
+++ is1/is.cc
@@ -4,74 +4,7 @@
 #include <iostream>
 #include "../common/vector.h"
 
-// For each pixel (x,y) and colour component c, we define the error error(y,x,c) as follows:
-//   Let colour(y,x,c) = data[c + 3 * x + 3 * nx * y].
-//   If (x,y) is located outside the rectangle: error(y,x,c) = outer[c] - colour(y,x,c).
-//   If (x,y) is located inside the rectangle: error(y,x,c) = inner[c] - colour(y,x,c).
-//   The total cost of the segmentation is the sum of squared errors, that is, the sum of error(y,x,c) * error(y,x,c) over all 0 <= c < 3 and 0 <= x < nx and 0 <= y < ny.
-
-
-// TASK IS1 Your task is to find a segmentation that minimises the total cost.
-
-// Implement an algorithm for image segmentation. You can use the naive algorithm that tries out all possible locations 0 <= y0 < y1 <= ny and 0 <= x0 < x1 <= nx for the rectangle and finds the best one.
-
-// However, your implementation has to be reasonably efficient. In particular, make sure that for each possible location of the rectangle you only need to perform O(1) operations to evaluate how good this position is. That is, the total complexity for an n × n figure has to be O(n4). See the hints for suggestions.
-
-// You are supposed to use the following technique to parallelise your code:
-
-//   Multiple threads with OpenMP: Different threads try out different candidate locations.
-//   Vector instructions: Store the colour image so that each pixel is a 4-element vector, with 3 colour components and a zero for padding. This way you can do all calculations for all colour components in parallel.
-
-// Examples of a sufficient performance with classroom computers:
-//   nx = ny = 400: roughly 10 seconds
-
 #define d4tod(d4) d4[0]+d4[1]+d4[2]+d4[3]
-
-double PRECISION = 0.0;
-double double_check_hXY(int x0, int y0, int x1, int y1, int nx, int ny, const float* data, double4_t* cdata) {
-  if (data) {};
-  double hXY = 0;
-  double4_t hXY4 = {0};
-  // double test_vXc = 0.0;
-  // double test_vYc = 0.0;
-  double4_t test_vXc4 = {0.0};
-  double4_t test_vYc4 = {0.0};
-
-  double sizeX = ((double)y1 - (double)y0) * ((double)x1 - (double)x0);
-  double sizeY = ((double)nx * (double)ny) - sizeX;
-  double divX = 1.0 / sizeX;
-  double divY = 1.0 / sizeY;
-
-  for (int j=0; j<ny; j++) {
-    for (int i=0; i<nx; i++) {
-      bool inside = y0 <= j && j < y1 && x0 <= i && i < x1;
-      double4_t d = cdata[(j*nx + i)];
-      if (inside) {
-	test_vXc4 += d;
-      } else {
-	test_vYc4 += d;
-      }
-    }
-  }
-
-  hXY4 = (test_vXc4 * test_vXc4 * divX) + (test_vYc4 * test_vYc4 * divY);
-  hXY = d4tod(hXY4);
-
-  return hXY;
-}
-
-void printS00(int ny, int nx, double* S00) {
-  printf("\n\nS00:\n");
-  printf("nx: %d, ny: %d\n", nx, ny);
-  for (int y=0; y<ny; y++) {
-    for (int x=0; x<nx; x++) {
-      printf("%f ", S00[y*nx + x]);
-    }
-    printf("\n");
-  }
-  printf("\n");
-  printf("END OF S00 PRINT:\n");
-}
 
 Result segment(int ny, int nx, const float* data) {
   int size = nx * ny; // size of the whole image
@@ -108,10 +41,6 @@ Result segment(int ny, int nx, const float* data) {
     }
   }
 
-  if ((nx == 100 && ny == 67) || (nx == 67 && ny == 100)) {
-    printS00(ny+1, nx+1, S00);
-  }
-
   //#pragma omp parallel for schedule(static, 1)
   for (int h=1; h<=ny; h++) {
     for (int w=1; w<=nx; w++) {
@@ -133,13 +62,6 @@ Result segment(int ny, int nx, const float* data) {
 	  double tmpX = vXc * vXc * divX;
 	  double tmpY = vYc * vYc * divY;
 	  hXY = tmpX + tmpY;
-
-	  //double test_hXY = double_check_hXY(x0, y0, x1, y1, nx, ny, data, cdata);
-	  //hXY = test_hXY;
-
-	  // if (std::abs(test_hXY - hXY) > EPSILON) {
-	  //   printf("ERROR!");
-	  // }
 
 	  if (hXY > max_hXY) {
 	    max_hXY = hXY;
