c98848c166d3817c05520d1e4c692f7222df28e2
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-04-29 21:41:40 +0300

    Not working parallelism

--- is1/is.cc
+++ is1/is.cc
@@ -3,16 +3,18 @@
 #include <stdio.h>
 #include <iostream>
 #include "../common/vector.h"
+#include <omp.h>
 
-#define d4tod(d4) d4[0]+d4[1]+d4[2]+d4[3]
+#define d4tod(d4) d4[0]+d4[1]+d4[2]
 
 Result segment(int ny, int nx, const float* data) {
   int size = nx * ny; // size of the whole image
   double4_t cdata[size] = {0.0}; // vectorized data
-  double4_t vPc4 = {0.0};
+  double4_t vPc = {0.0};
+  double maxmaxhXY = 0.0;
 
-  double vPc = 0.0, max_hXY = 0.0;
-  double S00[(ny+1)*(nx+1)] = {0.0};
+  int snx = nx+1, sny = ny+1;
+  double4_t S00[snx*sny] = {0.0};
 
   int ry0=0, rx0=0, ry1=1, rx1=1; // return coordinates
   double4_t rXc = {0.0}, rYc = {0.0}; // return colors
@@ -24,56 +26,72 @@ Result segment(int ny, int nx, const float* data) {
       for (int c=0; c<3; c++) {
 	cdata[idx][c] = data[c + 3*idx];
       }
-      vPc4 += cdata[idx];
+      vPc += cdata[idx];
     }
   }
 
-  vPc = d4tod(vPc4);
+  //vPc = d4tod(vPc4);
 
   // The borders (x=0 and y=0) for the color sum matrix
   // are 0. Thus we can dynamically calculate the other sums.
   for (int y1=0; y1<ny; y1++) {
     for (int x1=0; x1<nx; x1++) {
-      S00[(y1+1)*(nx+1) + (x1+1)] = d4tod(cdata[y1*nx + x1])
-                                  + S00[y1*(nx+1) + (x1+1)]
- 	                          + S00[(y1+1)*(nx+1) + x1]
-                                  - S00[y1*(nx+1) + x1];
+      S00[(y1+1)*snx + (x1+1)] = d4tod(cdata[y1*nx + x1])
+                               + S00[y1    *snx + (x1+1)]
+                               + S00[(y1+1)*snx + x1    ]
+                               - S00[y1    *snx + x1    ];
     }
   }
 
-  //#pragma omp parallel for schedule(static, 1)
+#pragma omp parallel
+{
+  double4_t hXY4, vYc, vXc;
+  double sizeX, sizeY, divX, divY, hXY, max_hXY = -1;
+  int tx0 = 0, ty0 = 0, tx1 = 1, ty1 = 1;
+
+  #pragma omp for schedule(static, 1)
   for (int h=1; h<=ny; h++) {
     for (int w=1; w<=nx; w++) {
-      double sizeX = (double)h * (double)w;
-      double sizeY = (double)size - sizeX;
-      double divX = 1.0/sizeX;
-      double divY = 1.0/sizeY;
+      sizeX = (double)h * (double)w;
+      sizeY = (double)size - sizeX;
+      divX = 1.0/sizeX;
+      divY = 1.0/sizeY;
 
       for (int y0=0; y0<=ny-h; y0++) {
 	for (int x0=0; x0<=nx-w; x0++) {
 	  int y1 = y0 + h;
 	  int x1 = x0 + w;
-	  double hXY, vYc, vXc;
-	  vXc = S00[y1*(nx+1) + x1]
-	      - S00[y1*(nx+1) + x0]
-	      - S00[y0*(nx+1) + x1]
-	      + S00[y0*(nx+1) + x0];
+	  vXc = S00[y1*snx + x1]
+	      - S00[y1*snx + x0]
+	      - S00[y0*snx + x1]
+	      + S00[y0*snx + x0];
 	  vYc = vPc - vXc;
-	  double tmpX = vXc * vXc * divX;
-	  double tmpY = vYc * vYc * divY;
-	  hXY = tmpX + tmpY;
+	  hXY4 = (vXc * vXc) * divX + (vYc * vYc) * divY;
+	  hXY = d4tod(hXY4);
 
 	  if (hXY > max_hXY) {
 	    max_hXY = hXY;
-	    rx0 = x0;
-	    ry0 = y0;
-	    rx1 = x1;
-	    ry1 = y1;
+	    tx0 = x0;
+	    ty0 = y0;
+	    tx1 = x1;
+	    ty1 = y1;
 	  }
 	}
       }
     }
   }
+
+  #pragma omp critical
+  {
+    if (max_hXY > maxmaxhXY) {
+      maxmaxhXY = max_hXY;
+      rx0 = tx0;
+      ry0 = ty0;
+      rx1 = tx1;
+      ry1 = ty1;
+    }
+  }
+}
 
   for (int y=0; y<ry0; y++) {
     for (int x=0; x<nx; x++) {
