e7df25b40965cf6e73d7e3cc82a1b7f00b3910b6
Hartikainen Kristian <kristian.hartikainen@aalto.fi>
2015-05-16 22:39:04 +0300

    working solution for cp9 for nice numbers

--- cp9/cp.cu
+++ cp9/cp.cu
@@ -60,55 +60,32 @@ __global__ void matrix_multiply(int nx, int ny, float* X, float* result) {
   int x = tx + bx * blockDim.x; // global x
   int y = ty + by * blockDim.y; // global y
 
-  int x_base = m*k*bx + ty*k; // y1
-  int y_base = m*k*by + ty*k; // y2
-
-
-  __syncthreads();
-  if (tx == 0 && ty == 0) printf("block at: y=%d, x=%d\n", by, bx);
-  __syncthreads();
-  printf("thread at point: y=%d, x=%d\n", y, x);
-  __syncthreads();
-  printf("txty: ty=%d, tx=%d\n", ty, tx);
-  __syncthreads();
+  int x_base = m*k*bx; // y1
+  int y_base = m*k*by; // y2
 
   // if (x < y || x >= ny || y >= ny) return;
 
   for (int mx=0; mx<nx; mx+=m) {
     for (int i=0; i<k; i++) {
-      // Are these correct??
-      A1[ty][tx][i] = X[(x_base + i)*nx + (mx + tx)];
-      A2[ty][tx][i] = X[(y_base + i)*nx + (mx + tx)];
-    }
 
-    if (tx == 0 && ty == 0) printf("printing cache, mx=%d, tx=%d, ty=%d", mx, tx, ty);
-    for (int j=0; j<m; j++) {
-      for (int kk=0; kk<k; kk++) {
-	printf("\n");
-	for (int i=0; i<m; i++) {
-	  if (tx == 0 && ty == 0) printf("%f\t", A1[j][i][kk]);
-	}
-      }
-      if (tx == 0 && ty == 0) printf("\n");
+      A1[ty][tx][i] = X[(x_base + ty*k + i)*nx + (mx + tx)];
+      A2[ty][tx][i] = X[(y_base + ty*k+ i)*nx + (mx + tx)];
     }
 
     __syncthreads();
 
     for (int mm=0; mm<m; mm++) {
-      if (tx == 0 && ty == 0) printf("mm=%d\n", mm);
       for (int i=0; i<k; i++) {
 	for (int j=0; j<k; j++) {
 	  tmp[j][i] += A1[tx][mm][i] * A2[ty][mm][j];
-	  if (tx == 0 && ty == 0) printf("%f\t", tmp[j][i]);
 	}
-	if (tx == 0 && ty == 0) printf("\n");
       }
     }
   }
 
   for (int i=0; i<k; i++) {
     for (int j=0; j<k; j++) {
-      result[(y_base + j) * ny + (x_base + i)] = tmp[j][i];
+      result[(y_base + ty*k + j) * ny + (x_base + tx*k + i)] = tmp[j][i];
     }
   }
 }
@@ -135,14 +112,6 @@ void correlate(int ny, int nx, const float* data, float* result) {
     }
   }
 
-  printf("\nnormalized data:\n");
-  for (int j=0; j<ny; j++) {
-    for (int i=0; i<nx; i++) {
-      std::cout << X[j*nx + i] << " ";
-    }
-    printf("\n");
-  }
-
   // Allocate GPU memory for X
   CHECK_CUDA_ERROR( cudaMalloc( (void**)&dev_X, ny * nx * sizeof(float) ) );
   CHECK_CUDA_ERROR( cudaMalloc( (void**)&dev_result, ny * ny * sizeof(float) ) );
@@ -166,15 +135,6 @@ void correlate(int ny, int nx, const float* data, float* result) {
   			       dev_result,
   			       sizeof(float)*ny*ny,
   			       cudaMemcpyDeviceToHost) );
-
-
-  printf("\nresult:\n");
-  for (int j=0; j<ny; j++) {
-    for (int i=0; i<ny; i++) {
-      std::cout << result[j*nx + i] << " ";
-    }
-    printf("\n");
-  }
 
   free(X);
   cudaFree(dev_X);
